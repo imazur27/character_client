55a7d5025ce66a76663c799c6cca40a969619290
ilya <fakeemail@fakeemail.com>
2025-04-14 08:57:07 +0300

    added doxygen comments
    added doxygen comments

--- character_client/add_character_dialog.h
+++ character_client/add_character_dialog.h
@@ -10,26 +10,76 @@ namespace Ui {
 class AddCharacterDialog;
 }
 
+/**
+ * \brief The AddCharacterDialog class provides a dialog for adding a new character.
+ *
+ * This dialog allows users to input character data and validates the input before
+ * accepting it. It inherits from QDialog and utilizes the Qt framework for its UI.
+ */
 class AddCharacterDialog : public QDialog {
     Q_OBJECT
 
 public:
+    /**
+     * \brief Constructs an AddCharacterDialog with the given parent widget.
+     *
+     * \param parent The parent widget for this dialog. Defaults to nullptr.
+     */
     explicit AddCharacterDialog(QWidget* parent = nullptr);
+
+    /**
+     * \brief Destroys the AddCharacterDialog instance.
+     */
     ~AddCharacterDialog();
 
+    /**
+     * \brief Retrieves the character data entered in the dialog.
+     *
+     * \return CharacterData The character data populated from the dialog fields.
+     */
     CharacterData getCharacterData() const;
 
 private slots:
+    /**
+     * \brief Overrides the accept method to handle dialog acceptance.
+     *
+     * This method is called when the user confirms the dialog (e.g., clicks OK).
+     */
     void accept() override;
 
 private:
+    /**
+     * \brief Displays an error message next to the specified widget.
+     *
+     * \param widget The widget next to which the error message will be displayed.
+     * \param message The error message to display.
+     */
     void showError(QWidget* widget, const QString& message);
+
+    /**
+     * \brief Clears any error message displayed next to the specified widget.
+     *
+     * \param widget The widget from which the error message will be cleared.
+     */
     void clearError(QWidget* widget);
+
+    /**
+     * \brief Validates the character name input.
+     *
+     * \param name The character name to validate.
+     * \return true if the name is valid; false otherwise.
+     */
     bool validateName(const QString& name);
+
+    /**
+     * \brief Validates the entire form before submission.
+     *
+     * \return true if the form is valid; false otherwise.
+     */
     bool validateForm();
 
-    Ui::AddCharacterDialog* ui;
-    QMap<QWidget*, QLabel*> m_errorLabels{};
+    Ui::AddCharacterDialog* ui; ///< Pointer to the UI elements of the dialog.
+    QMap<QWidget*, QLabel*> m_errorLabels; ///< Map of widgets to their corresponding error labels.
 };
 
 #endif // ADD_CHARACTER_DIALOG_H
--- character_client/character_info_dialog.h
+++ character_client/character_info_dialog.h
@@ -1,6 +1,6 @@
 /**
- * @file character_info_dialog.h
- * @brief Dialog for displaying and editing character information
+ * \file character_info_dialog.h
+ * \brief Dialog for displaying and editing character information
  */
 
 #ifndef CHARACTER_INFO_DIALOG_H
@@ -14,31 +14,73 @@ class CharacterInfoDialog;
 }
 
 /**
- * @class CharacterInfoDialog
- * @brief Modal dialog for viewing and editing character details
+ * \class CharacterInfoDialog
+ * \brief Modal dialog for viewing and editing character details
  */
 class CharacterInfoDialog : public QDialog {
     Q_OBJECT
 
 public:
+    /**
+     * \brief Constructs a CharacterInfoDialog with the specified character data.
+     *
+     * \param character The character data to be displayed and edited in the dialog.
+     * \param parent The parent widget for this dialog. Defaults to nullptr.
+     */
     explicit CharacterInfoDialog(const CharacterData& character, QWidget* parent = nullptr);
+
+    /**
+     * \brief Destroys the CharacterInfoDialog instance.
+     */
     ~CharacterInfoDialog();
 
+    /**
+     * \brief Retrieves the character data after editing.
+     *
+     * \return CharacterData The character data populated from the dialog fields.
+     */
     CharacterData getCharacterData() const;
 
 signals:
+    /**
+     * \brief Signal emitted when a remove request is made for a character.
+     *
+     * \param id The ID of the character to be removed.
+     */
     void signalRemoveRequested(int id);
+
+    /**
+     * \brief Signal emitted when an update request is made for a character.
+     *
+     * \param character The updated character data.
+     */
     void signalUpdateRequested(const CharacterData& character);
 
 private slots:
+    /**
+     * \brief Slot called when the update button is clicked.
+     *
+     * This method handles the logic for updating the character information.
+     */
     void slotUpdateClicked();
+
+    /**
+     * \brief Slot called when the remove button is clicked.
+     *
+     * This method handles the logic for removing the character.
+     */
     void slotRemoveClicked();
 
 private:
+    /**
+     * \brief Validates the input data in the dialog.
+     *
+     * \return true if the input is valid; false otherwise.
+     */
     bool validateInput();
 
-    Ui::CharacterInfoDialog* ui;
-    CharacterData m_character;
+    Ui::CharacterInfoDialog* ui; ///< Pointer to the UI elements of the dialog.
+    CharacterData m_character; ///< The character data being edited.
 };
 
 #endif // CHARACTER_INFO_DIALOG_H
